d7b7274b7b2cffb8f0dc970966cec0df12dd6b49
MarcoVacchi <mvacchi816@gmail.com>
2025-03-26 14:18:07 +0100

    feat: added send

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    console.log("Server del mio blog")
+    console.log("Server del mio blog");
+    res.send("Server del mio blogaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 });
 
+app.listen(port, () => {
 
+})
 //console.log("Server del mio ciao")
