26a9811663a48ddd43a6b3832e117a4473ca2296
MarcoVacchi <mvacchi816@gmail.com>
2025-03-26 13:57:00 +0100

    feat: create first console.log

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+    console.log("Server del mio blog")
+});
+
+
+//console.log("Server del mio ciao")
