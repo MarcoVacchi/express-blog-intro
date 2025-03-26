377f59e32a0d08e673140f68e330ba916d07d984
MarcoVacchi <mvacchi816@gmail.com>
2025-03-26 14:27:14 +0100

    feat: create rotta 2

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ app.get("/", (req, res) => {
     res.send("Server del mio blogaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 });
 
+// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare
+//  titolo, contenuto, immagine e tags (tags è un array di stringhe)
+
+
+app.get("/bacheca", (req, res) => {
+    //console.log("bacheca del mio blog"); prova per vedere se funziona questa rotta;
+    //res.send("bacheca del mio blogaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+// const dolci = [
+//     {
+//         name: 
+//     }
+// ];
+});
+
+
 app.listen(port, () => {
+    console.log("sono un server attivo sulla porta"+ " " + port);
 
 })
+
 //console.log("Server del mio ciao")
